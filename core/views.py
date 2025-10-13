ee0bcbab3aedf43058cbbb31c6220169bc2677da
avgdestr <leemkinyua@gmail.com>
2025-10-13 16:10:03 +0300

    login tokens

--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from .models import Artist, Artwork
 from .serializers import ArtistSerializer, ArtworkSerializer, LoginSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
 # -------------------------------
 # Artist Registration
